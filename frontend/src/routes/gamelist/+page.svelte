89d517d95b8aaeaeb15107e0661246e91e80ff1e
Raferat <mailnaneduveryhodnestranky@seznam.cz>
2025-01-01 18:09:27 +0100

    Fix error

--- frontend/src/routes/gamelist/+page.svelte
+++ frontend/src/routes/gamelist/+page.svelte
@@ -1,14 +1,7 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
-    import { type ApiError, type Game } from "$lib/api";
+    import { listGames, type ApiError, type Game } from "$lib/api";
     import { filterList, type Filters } from "./filter";
-
-    let {
-        data,
-    }: {
-        data: {gameData: Promise<[Game[], ApiError | undefined]>}
-    } = $props();
-
 
     let list: Game[] | undefined = $state();
     let filter: Filters = $state({});
@@ -27,7 +20,8 @@
     }
 
     $effect(()=>{
-        loadGameData(data.gameData);
+        const gameData = listGames(fetch);
+        loadGameData(gameData);
     })
 </script>
 
